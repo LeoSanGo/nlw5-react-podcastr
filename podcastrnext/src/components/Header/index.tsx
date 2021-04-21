31420dffff20e111266553a349416faeb59bf695
LeoSanGo <souoleo@gmail.com>
2021-04-21 10:28:11 -0300

    Criado componente player, adicionado a biblioteca date-fns

--- podcastrnext/src/components/Header/index.tsx
+++ podcastrnext/src/components/Header/index.tsx
@@ -1,10 +1,17 @@
-export function Header() {
-    return (
-        <header>
-            <img src="/logo.svg" alt="Podcastr"/>
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+import styles from "./styles.module.scss";
 
-            <p>O melhor para voce ouvir, sempre"</p>
-            <span>Qui, 8 Abril</span>
-        </header>
-    );
+export function Header() {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
+    locale: ptBR,
+  });
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr" />
+
+      <p>O melhor para voce ouvir, sempre"</p>
+      <span>{currentDate}</span>
+    </header>
+  );
 }
