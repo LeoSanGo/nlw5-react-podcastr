1fd10754f86e5a18b3c92f0e89e76009db363fba
LeoSanGo <souoleo@gmail.com>
2021-04-21 00:25:27 -0300

    Criação componente Header

---
+++ podcastrnext/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <header>
+            <img src="/logo.svg" alt="Podcastr"/>
+
+            <p>O melhor para voce ouvir, sempre"</p>
+            <span>Qui, 8 Abril</span>
+        </header>
+    );
+}
