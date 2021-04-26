51662ada0cca2d16957495425a83410c8521f80b
LeoSanGo <souoleo@gmail.com>
2021-04-26 01:01:34 -0300

    Recursos Shuffler, repeat e ajustes no scss

--- podcastrnext/src/pages/_app.tsx
+++ podcastrnext/src/pages/_app.tsx
@@ -4,31 +4,12 @@ import { Header } from "../components/Header";
 import { Player } from "../components/Player";
 
 import styles from "../styles/app.module.scss";
-import { PlayerContext } from "../context/PlayerContext";
-import { useState } from "react";
+import { PlayerContextProvider } from "../context/PlayerContext";
+
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setisPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setisPlaying(true);
-  
-  }
-
-  function togglePlay() {
-    setisPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setisPlaying(state);
-  }
-
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -36,7 +17,7 @@ function MyApp({ Component, pageProps }) {
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   );
 }
 
--- podcastrnext/src/pages/episodes/[slug].tsx
+++ podcastrnext/src/pages/episodes/[slug].tsx
@@ -5,9 +5,11 @@ import { api } from "../../services/api";
 import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 import styles from "./episode.module.scss";
+import { usePlayer } from "../../context/PlayerContext";
+import React from "react";
 
 type Episode = {
   id: string;
@@ -26,10 +28,13 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
-  
+  const { play } = usePlayer();
 
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
@@ -42,7 +47,7 @@ export default function Episode({ episode }: EpisodeProps) {
           src={episode.thumbnail}
           objectFit="cover"
         />
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódios" />
         </button>
       </div>
